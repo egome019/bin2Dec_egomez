6eeebf5d7ec1bf783c9c98f7531c1fe23dc98056
erik G <egomezfax19@gmail.com>
2020-12-21 14:45:40 -0800

    added some css and started js functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code goes here....
+const inputField = document.getElementById("inputField");
+const list = document.getElementById("dynamic");
+
+const button = document.querySelector("button");
+
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("hello");
+    convert();
+})
+
+const convert = () => {
+    const number = inputField.value;
+    const numArr = number.split("");
+    console.log(numArr);
+    const numList = document.createElement("ul");
+    for (let index = 0; index < numArr.length; index++) {
+        const binary = parseInt(numArr[index], 10);
+        list.innerHTML = binary;
+        
+        // const li = document.createElement("li");
+        // numList.appendChild(li);
+        // list.appendChild(numList);
+    }
+
+}
