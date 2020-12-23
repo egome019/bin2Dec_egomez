e6c5bcd1c7169c2adeba067ba0ddf6d80cffa4bc
erik G <egomezfax19@gmail.com>
2020-12-23 12:55:36 -0800

    added number validation

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const list = document.getElementById("dynamic");
 
 const button = document.querySelector("button");
 
+const handleChange = () => {
+    const number = inputField.value;
+    number.split("").map((digit) => {
+        if (digit !== "1" && digit !== "0") {
+            alert("Binary numbers only have 1s or 0s, please use any combination of 1s and 0s.");
+        }
+    })
+}
+
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("hello");
+    inputField.oninput = handleChange();
     convert();
 })
 
 const convert = () => {
     const number = inputField.value;
     const binary = parseInt(number, 2);
-    console.log(binary)
     const numList = document.createElement("ul");
     const li = document.createElement("li");
     list.appendChild(numList);
